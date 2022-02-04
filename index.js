005f658db2b998b7286b5c29c3871f28ceba17a3
Nataliia Dovha <natadnepr027@gmail.com>
2022-02-04 19:57:53 +0200

    cors

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const feedbackRouter = require('./routes/feedback.routes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json())
 
 app.use('/api', feedbackRouter)
