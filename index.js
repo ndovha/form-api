df29b03a62344b726891c9dcd01ffd9e3b5928ee
Nataliia Dovha <natadnepr027@gmail.com>
2022-02-04 20:10:36 +0200

    cors

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const feedbackRouter = require('./routes/feedback.routes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+  });
+  
 app.use(cors({
     origin: '*'
 }));
