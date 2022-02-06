efe7a1ab8b162967c9ed15fd2a4366b30890f9d3
Nataliia Dovha <natadnepr027@gmail.com>
2022-02-07 00:16:55 +0200

    update cors

--- index.js
+++ index.js
@@ -4,17 +4,9 @@ const cors = require('cors');
 const feedbackRouter = require('./routes/feedback.routes');
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-  });
   
 app.use(cors({
-    origin: '*'
+    origin: process.env.FRONT_URI || 'http://localhost:3000'
 }));
 app.use(express.json())
 
